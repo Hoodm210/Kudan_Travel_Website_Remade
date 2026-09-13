721e1e7bd0fca5c103c50e6da183f52ee03585fe
Hoodm210 <hoodm210@gmail.com>
2026-09-13 15:54:08 +0545

    refactor(about): optimize 3D canvas cleanup, accessibility, and image fallback handling

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"pQdCkIdZBoopaPs3zFO3ZeMfF75q/73WZMmBVPezoKY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"H6vQo07n76FwiAwlYhQq3v77dfdkvUocyxaA/N96fq4=\"\n}"
